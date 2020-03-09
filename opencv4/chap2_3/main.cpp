3fc2bb568327ac6354778ab9b1edff67108c838a
sikyung <sikoung0129@naver.com>
2020-03-09 19:55:26 +0900

    chap2_3 예제 - using namespace  사용한 소스코드

---
+++ opencv4/chap2_3/main.cpp
@@ -0,0 +1,25 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+
+using namespace cv;
+using namespace std;
+
+int main(void)
+{
+	std::cout << "hello opencv" << CV_VERSION << std::endl;
+
+	Mat img;
+	img = imread("lenna.bmp");
+
+	if (img.empty()) {
+		cerr << "image load failed!!" << endl;
+		return -1;
+	}
+
+	namedWindow("image");
+	imshow("image", img);
+
+	waitKey();
+	return 0;
+}
